aac5d325a0311b19476bc7b097318566c5918265
João Pedro Duarte <jurbasgames@gmail.com>
2024-07-30 13:41:04 -0300

    Add image lazy loading for better performance

--- django/app/models.py
+++ django/app/models.py
@@ -1,9 +1,12 @@
 from django.db import models
-import uuid
+from django.core.validators import MinValueValidator
+from PIL import Image as PilImage
+from django.core.files.base import ContentFile
+import io
 
 
 class Label(models.Model):
-    language = models.CharField(max_length=10)
+    language = models.CharField(max_length=10, db_index=True)
     value = models.CharField(max_length=255)
 
     def __str__(self):
@@ -15,10 +18,12 @@ class Manifest(models.Model):
         default="http://iiif.io/api/presentation/3/context.json")
     label = models.ManyToManyField(Label, related_name="manifest_labels")
     summary = models.ManyToManyField(Label, related_name="manifest_summaries")
-    items = models.ManyToManyField('Canvas', related_name='manifests')
+    items = models.ManyToManyField(
+        'Canvas', related_name='manifests', blank=True)
 
     def __str__(self):
-        return f"{self.id} {self.label.first().value}"
+        label = self.label.first()
+        return f"{self.id} {label.value if label else 'No Label'}"
 
 
 class Metadata(models.Model):
@@ -36,15 +41,19 @@ class Canvas(models.Model):
     height = models.IntegerField()
     width = models.IntegerField()
     annotation_pages = models.ManyToManyField(
-        'AnnotationPage', related_name="canvas_items")
-    painting_annotations = models.ManyToManyField(
-        'Annotation', related_name="canvas_painting_annotations")
+        'AnnotationPage', related_name="canvas_items", blank=True)
+    images = models.ManyToManyField(
+        'Image',
+        related_name='canvas_images',
+        blank=True
+    )
 
     class Meta:
         verbose_name_plural = "Canvases"
 
     def __str__(self):
-        return self.id
+        label = self.label.first()
+        return f"{self.id} {label.value if label else 'No Label'}"
 
 
 class AnnotationPage(models.Model):
@@ -52,26 +61,58 @@ class AnnotationPage(models.Model):
         'Annotation', related_name="annotation_page_items")
 
     def __str__(self):
-        return self.id
-
-
-class Image(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    label = models.ManyToManyField(Label, related_name="image_labels")
-    format = models.CharField(max_length=50)
-    height = models.IntegerField()
-    width = models.IntegerField()
-
-    def __str__(self):
-        return self.id
+        return str(self.id)
 
 
 class Annotation(models.Model):
     target = models.ForeignKey(
-        'Canvas', on_delete=models.CASCADE, related_name='target_annotations')
-    body = models.ForeignKey(
-        Image, on_delete=models.CASCADE, related_name='annotations_as_body')
-    motivation = models.CharField(max_length=255)
+        Canvas, on_delete=models.CASCADE, related_name='annotations')
+    label = models.ManyToManyField(Label, related_name="annotation_labels")
+    motivation = models.CharField(max_length=255, default='commenting', choices=[(
+        'comment', 'Comentário'), ('highlight', 'Destaque'), ('scanning', 'Digitalização')])
+    body_text = models.TextField(null=True, blank=True)
 
     def __str__(self):
-        return self.id
+        return f"Annotation {self.id} on {self.target.id}"
+
+
+class Image(models.Model):
+    canvases = models.ManyToManyField(
+        Canvas,
+        blank=True
+    )
+    format = models.CharField(
+        max_length=50, db_index=True, blank=True)
+    height = models.IntegerField(
+        validators=[MinValueValidator(1)], editable=False, null=True, blank=True)
+    width = models.IntegerField(
+        validators=[MinValueValidator(1)], editable=False, null=True, blank=True)
+    file = models.FileField(upload_to='data/images/', null=True, blank=True)
+
+    def __str__(self):
+        canvas_ids = ", ".join(str(canvas.id)
+                               for canvas in self.canvases.all())
+        return f"Image {self.id} on Canvas {canvas_ids if canvas_ids else 'No Canvas'}"
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            super(Image, self).save(*args, **kwargs)
+
+        if self.file:
+            img = PilImage.open(self.file)
+            self.width, self.height = img.size
+            if not self.format:
+                self.format = img.format
+
+            new_filename = f"{self.id}.{self.format.lower()}"
+
+            buffer = io.BytesIO()
+            img.save(buffer, format=self.format)
+            file_content = ContentFile(buffer.getvalue())
+
+            self.file.delete(save=False)
+            self.file.save(new_filename, file_content, save=False)
+
+            buffer.close()
+
+        super(Image, self).save(*args, **kwargs)
