5d89eb8dee82c12a5affa571675c2f218bb92758
João Pedro Duarte <jurbasgames@gmail.com>
2024-07-30 18:42:40 -0300

    Improve admin

--- django/app/admin.py
+++ django/app/admin.py
@@ -3,9 +3,40 @@ from .models import Manifest, Canvas, Image, Label, Metadata, TextAnnotation
 
 # Register your models here.
 
-admin.site.register(Manifest)
-admin.site.register(Canvas)
-admin.site.register(Image)
-admin.site.register(Label)
-admin.site.register(Metadata)
-admin.site.register(TextAnnotation)
+
+@admin.register(Manifest)
+class ManifestAdmin(admin.ModelAdmin):
+    list_display = ('id', 'get_label')
+
+    def get_label(self, obj):
+        return obj.label.first().value if obj.label.exists() else 'No Label'
+    get_label.short_description = 'Label'
+
+
+@admin.register(Canvas)
+class CanvasAdmin(admin.ModelAdmin):
+    list_display = ('id', 'get_label', 'height', 'width')
+
+    def get_label(self, obj):
+        return obj.label.first().value if obj.label.exists() else 'No Label'
+    get_label.short_description = 'Label'
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'format', 'height', 'width')
+
+
+@admin.register(Label)
+class LabelAdmin(admin.ModelAdmin):
+    list_display = ('language', 'value')
+
+
+@admin.register(Metadata)
+class MetadataAdmin(admin.ModelAdmin):
+    list_display = ('manifest',)
+
+
+@admin.register(TextAnnotation)
+class TextAnnotationAdmin(admin.ModelAdmin):
+    list_display = ('motivation', 'text', 'language')
