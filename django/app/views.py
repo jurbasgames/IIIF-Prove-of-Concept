c9c7e94e7f279ce8366324508fcce1b9c5580f79
João Pedro Duarte <jurbasgames@gmail.com>
2024-07-30 18:36:06 -0300

    Rename image_id to manifest_id

--- django/app/views.py
+++ django/app/views.py
@@ -1,7 +1,7 @@
-from django.http import JsonResponse, HttpResponse
+from django.http import HttpResponse
 from .utils import create_manifest
 
 
-def manifest_view(request, image_id):
-    manifest_json = create_manifest(image_id)
+def manifest_view(request, manifest_id):
+    manifest_json = create_manifest(manifest_id)
     return HttpResponse(manifest_json, content_type='application/json')
