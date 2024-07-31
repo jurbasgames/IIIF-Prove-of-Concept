a73cb874b0e8f1ab9680040a18a0986b73df21c0
Jurbas Duarte <jurbasgames@gmail.com>
2024-07-31 15:00:19 -0300

    Update database host in settings.py for docker

--- django/digital_collection/settings.py
+++ django/digital_collection/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME', 'iiif_collection'),
         'USER': os.getenv('DB_USER', 'root'),
         'PASSWORD': os.getenv('DB_PASSWORD', 'root'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'HOST': os.getenv('DB_HOST', 'db'),
         'PORT': os.getenv('DB_PORT', '3306'),
     }
 }
